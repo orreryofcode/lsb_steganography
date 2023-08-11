f282354a399fe1e124036a49d7b61c06caa4c3d5
Raul Menendez <orreryofcode@gmail.com>
2023-08-10 18:02:31 -0700

    semicolon genocide

--- decoder.py
+++ decoder.py
@@ -1,73 +1,67 @@
 from PIL import Image
 
-original_img_bins = [];
-decoded_bins = [];
+original_img_bins = []
+decoded_bins = []
+
 
 def get_ascii():
-    message = "";
+    message = ""
     for i in range(len(decoded_bins)):
-        ascii_code = int(decoded_bins[i], 2);
+        ascii_code = int(decoded_bins[i], 2)
         if ascii_code == 126:
             break
         else:
-            message += chr(ascii_code);
+            message += chr(ascii_code)
 
-    print(message);
+    print(message)
+
 
 def get_lsb():
-
-    count = 0;
+    count = 0
     while count < len(original_img_bins):
-        chunk_a = original_img_bins[count];
+        chunk_a = original_img_bins[count]
 
-        chunk_b = original_img_bins[count + 1];
+        chunk_b = original_img_bins[count + 1]
 
-        chunk_c = original_img_bins[count + 2];
+        chunk_c = original_img_bins[count + 2]
 
         completed_chunk = chunk_a[len(chunk_a) - 3:] + chunk_b[len(chunk_b) - 3:] + chunk_c[len(chunk_c) - 2:]
-        decoded_bins.append(completed_chunk);
+        decoded_bins.append(completed_chunk)
 
-        count = count + 3;
+        count = count + 3
+
 
 def rgb_to_bin(rgb_value):
-
     # Convert RGB value to binary
     for color in rgb_value:
-        binary_value = bin(color)[2:];
+        binary_value = bin(color)[2:]
 
         # Add padding for MSB
         if len(binary_value) < 8:
-            binary_value = binary_value.rjust(8, '0');
+            binary_value = binary_value.rjust(8, '0')
 
+        original_img_bins.append(binary_value)
 
-        original_img_bins.append(binary_value);
 
 def get_rgb_value_of_img(img_path):
-    img = Image.open(img_path);
-    img_w = img.width; # should be img.width
-    img_h = img.height; #should be img.height
+    img = Image.open(img_path)
+    img_w = img.width
+    img_h = img.height
 
-    row = 0; # Top left Corner of Image;
+    row = 0  # Top left Corner of Image;
 
     while row < img_w:
-        col = 0;
+        col = 0
 
         while col < 1:
-            rgb_val = img.getpixel((row, col));
+            rgb_val = img.getpixel((row, col))
 
-            rgb_to_bin(rgb_val);
-            col = col + 1;
+            rgb_to_bin(rgb_val)
+            col = col + 1
 
-        row = row + 1;
+        row = row + 1
 
-get_rgb_value_of_img("modified.png");
+
+get_rgb_value_of_img("modified.png")
 get_lsb()
 get_ascii()
-
-
-
-
-
-
-
-
--- imgproc.py
+++ imgproc.py
@@ -1,65 +1,66 @@
 from PIL import Image
 
-original_img_bins = [];
-message_ascii_vals = [];
-message_bin_vals = [];
-message_chunks = [];
-new_bin_values = [];
-new_rgb_values = [];
+original_img_bins = []
+message_ascii_vals = []
+message_bin_vals = []
+message_chunks = []
+new_bin_values = []
+new_rgb_values = []
 
 
 def pixel_replacer(img_path):
-    img = Image.open(img_path); # open image
-    pixelMap = img.load();  # load pixel map
-    img_w = img.width;
-    img_h = img.height;
-    rgb = [];
+    img = Image.open(img_path)  # open image
+    pixelMap = img.load()  # load pixel map
+    img_w = img.width
+    img_h = img.height
+    rgb = []
 
-    i = 0;
-    length = len(new_rgb_values);
+    i = 0
+    length = len(new_rgb_values)
     for value in new_rgb_values:
         if i < length:
-            chunk_a = new_rgb_values[i];
-            chunk_b = new_rgb_values[i+1];
-            chunk_c = new_rgb_values[i+2];
+            chunk_a = new_rgb_values[i]
+            chunk_b = new_rgb_values[i + 1]
+            chunk_c = new_rgb_values[i + 2]
 
-            rgb.append((chunk_a, chunk_b, chunk_c));
+            rgb.append((chunk_a, chunk_b, chunk_c))
 
-        i = i + 3;
+        i = i + 3
 
-    count = 0;
+    count = 0
     for j in range(img_w):
         for k in range(img_h):
             if count < len(rgb):
-                pixelMap[k,j] = rgb[count];
+                pixelMap[k, j] = rgb[count]
 
-                count = count + 1;
+                count = count + 1
             else:
-                break;
+                break
 
-
-
-    img.save("modified.png");
+    img.save("modified.png")
     print("Modified image was saved.")
+
 
 def new_colors():
     for value in new_bin_values:
-        color = int(value, 2);
-        new_rgb_values.append(color);
+        color = int(value, 2)
+        new_rgb_values.append(color)
+
 
 def lsb_replacer():
-    start = 0;
+    start = 0
     for value in message_chunks:
-        idx = message_chunks.index(value, start);
-        value_length = len(value);
+        idx = message_chunks.index(value, start)
+        value_length = len(value)
 
-        rgb_bin = original_img_bins[idx];
-        new_rgb = rgb_bin[:len(rgb_bin) - value_length];
+        rgb_bin = original_img_bins[idx]
+        new_rgb = rgb_bin[:len(rgb_bin) - value_length]
 
-        new_rgb = new_rgb + value;
+        new_rgb = new_rgb + value
 
-        new_bin_values.append(new_rgb);
-        start = start + 1;
+        new_bin_values.append(new_rgb)
+        start = start + 1
+
 
 def chunkify():
     for value in message_bin_vals:
@@ -67,64 +68,61 @@ def chunkify():
         message_chunks.append(value[3:6])
         message_chunks.append(value[6:])
 
+
 def text_ascii_to_bin():
     for code in message_ascii_vals:
         processed = bin(code)[2:]
-        message_bin_vals.append(processed.rjust(8,'0'));
+        message_bin_vals.append(processed.rjust(8, '0'))
 
-    chunkify();
+    chunkify()
+
 
 def text_to_ascii(text):
     for c in text:
-        message_ascii_vals.append(ord(c));
+        message_ascii_vals.append(ord(c))
+
 
 def rgb_to_bin(rgb_value):
-
     # Convert RGB value to binary
     for color in rgb_value:
-        binary_value = bin(color)[2:];
+        binary_value = bin(color)[2:]
 
         # Add padding for MSB
         if len(binary_value) < 8:
-            binary_value = binary_value.rjust(8, '0');
+            binary_value = binary_value.rjust(8, '0')
 
+        original_img_bins.append(binary_value)
 
-        original_img_bins.append(binary_value);
 
 def get_rgb_value_of_img(img_path):
-    img = Image.open(img_path);
+    img = Image.open(img_path)
 
-    img_w = img.width; # should be img.width
-    img_h = img.height; #should be img.height
+    img_w = img.width
+    img_h = img.height
 
-    row = 0; # Top left Corner of Image;
+    row = 0  # Top left Corner of Image;
 
     while row < img_w:
-        col = 0;
+        col = 0
 
         while col < img_h:
-            rgb_val = img.getpixel((row, col));
-            rgb_to_bin(rgb_val);
-            col = col + 1;
+            rgb_val = img.getpixel((row, col))
+            rgb_to_bin(rgb_val)
+            col = col + 1
 
-        row = row + 1;
+        row = row + 1
 
-    img.close();
+    img.close()
+
 
 def init(original_file, message):
-    get_rgb_value_of_img(original_file);
-    test_string = message + "~";
-    text_to_ascii(test_string);
-    text_ascii_to_bin();
-    lsb_replacer();
-    new_colors();
-    pixel_replacer(original_file);
+    get_rgb_value_of_img(original_file)
+    test_string = message + "~"
+    text_to_ascii(test_string)
+    text_ascii_to_bin()
+    lsb_replacer()
+    new_colors()
+    pixel_replacer(original_file)
 
 
-init("gato.jpg", "This cat is not a cat but a picture of a cat.");
-
-
-
-
-
-
+init("gato.jpg", "Secret Message... Shhhh")
